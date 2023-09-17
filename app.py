ccdfce197f03844436d78716fe033adffb474dc6
Kishor-Lahade <92206744+Kishor-Lahade@users.noreply.github.com>
2023-09-18 00:14:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+import gradio as gr
+
+from transformers import LineByLineTextDataset
+from transformers import DataCollatorForLanguageModeling
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from transformers import Trainer, TrainingArguments
+
+def load_model(model_path):
+    model = GPT2LMHeadModel.from_pretrained(model_path)
+    return model
+
+
+def load_tokenizer(tokenizer_path):
+    tokenizer = GPT2Tokenizer.from_pretrained(tokenizer_path)
+    return tokenizer
+
+model_path = r'output'
+model = load_model(model_path)
+tokenizer = load_tokenizer(model_path)
+
+
+def generate_text(sequence, max_new_tokens):
+    ids = tokenizer.encode(f'{sequence}', return_tensors='pt')
+    input_length = ids.size(1)
+    max_length = input_length + max_new_tokens
+    final_outputs = model.generate(
+        ids,
+        do_sample=True,
+        max_length=max_length,
+        pad_token_id=model.config.eos_token_id
+    )
+    return tokenizer.decode(final_outputs[0], skip_special_tokens=True)
+
+
+output = []
+
+def root(prompt: str):
+    print("prompt " +prompt)
+    output = [generate_text("Prompt: " + prompt + " Response: ", 7).split('Response: ')[1]]
+    print(output)
+    return generate_text("Prompt: " + prompt + " Response: ", 7).split('Response: ')[1]
+
+
+iface1 = gr.Interface(fn=root, inputs="text", outputs="text")
+iface1.launch()
